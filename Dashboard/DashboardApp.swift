d28441da4272a8305f3139068fdd94fcc2eabbe1
Chris Ross <cross@distal.com>
2023-04-08 19:21:41 -0400

    Initial Commit

---
+++ Dashboard/DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardApp.swift
+//  Dashboard
+//
+//  Created by Chris Ross on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
